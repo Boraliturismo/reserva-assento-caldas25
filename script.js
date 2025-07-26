911d459afeeb11100408bfd3907bd04874ebae9c
Boraliturismo <agenciaborali.turismo@gmail.com>
2025-07-26 13:17:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,142 +1,91 @@
 var firstSeatLabel = 1;
-var booked = !!localStorage.getItem('booked') ? $.parseJSON(localStorage.getItem('booked')) : [];
-$(document).ready(function() {
-    var $cart = $('#selected-seats'),
-        $counter = $('#counter'),
-        $total = $('#total'),
-        sc = $('#bus-seat-map').seatCharts({
-            map: [
-                'ff_ff',
-                'ff_ff',
-                'ee_ee',
-                'ee_ee',
-                'ee___',
-                'ee_ee',
-                'ee_ee',
-                'ee_ee',
-                'eeeee',
-            ],
-            seats: {
-                f: {
-                    price: 75,
-                    classes: 'first-class', //your custom CSS class
-                    category: 'First Class'
-                },
-                e: {
-                    price: 50,
-                    classes: 'economy-class', //your custom CSS class
-                    category: 'Economy Class'
-                }
+$(document).ready(function () {
+  var $carrinho = $('#selected-seats'),
+      $contador = $('#counter'),
+      $total = $('#total'),
+      sc = $('#bus-seat-map').seatCharts({
+          map: [
+              'ff_ff',
+              'ff_ff',
+              'ee_ee',
+              'ee_ee',
+              'ee___',
+              'ee_ee',
+              'ee_ee',
+              'ee_ee',
+              'eeeee',
+          ],
+          seats: {
+              f: {
+                  price: 75,
+                  classes: 'mais-conforto',
+                  category: 'Mais conforto'
+              },
+              e: {
+                  price: 50,
+                  classes: 'assento-padrao',
+                  category: 'Assento Padrão'
+              }
+          },
+          naming: {
+              top: false,
+              getLabel: function () {
+                  return firstSeatLabel++;
+              }
+          },
+          legend: {
+              node: $('#legend'),
+              items: [
+                  ['f', 'available', 'Mais conforto'],
+                  ['e', 'available', 'Assento Padrão'],
+                  ['f', 'unavailable', 'Não disponível']
+              ]
+          },
+          click: function () {
+              if (this.status() == 'available') {
+                  if (!$('#nome').val() || !$('#rg').val()) {
+                      alert("Por favor, preencha o nome completo e o RG antes de continuar.");
+                      return 'available';
+                  }
+                  $('<li>' + this.data().category + ' - Assento nº ' + this.settings.label +
+                      ': <b>R$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancelar]</a></li>')
+                      .attr('id', 'cart-item-' + this.settings.id)
+                      .data('seatId', this.settings.id)
+                      .appendTo($carrinho);
+                  $contador.text(sc.find('selected').length + 1);
+                  $total.text(recalcularTotal(sc) + this.data().price);
+                  return 'selected';
+              } else if (this.status() == 'selected') {
+                  $contador.text(sc.find('selected').length - 1);
+                  $total.text(recalcularTotal(sc) - this.data().price);
+                  $('#cart-item-' + this.settings.id).remove();
+                  return 'available';
+              } else if (this.status() == 'unavailable') {
+                  return 'unavailable';
+              } else {
+                  return this.style();
+              }
+          }
+      });
 
-            },
-            naming: {
-                top: false,
-                getLabel: function(character, row, column) {
-                    return firstSeatLabel++;
-                },
-            },
-            legend: {
-                node: $('#legend'),
-                items: [
-                    ['f', 'available', 'First Class'],
-                    ['e', 'available', 'Economy Class'],
-                    ['f', 'unavailable', 'Already Booked']
-                ]
-            },
-            click: function() {
-                if (this.status() == 'available') {
-                    //let's create a new <li> which we'll add to the cart items
-                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
-                        .attr('id', 'cart-item-' + this.settings.id)
-                        .data('seatId', this.settings.id)
-                        .appendTo($cart);
-
-                    /*
-                     * Lets update the counter and total
-                     *
-                     * .find function will not find the current seat, because it will change its stauts only after return
-                     * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
-                     */
-                    $counter.text(sc.find('selected').length + 1);
-                    $total.text(recalculateTotal(sc) + this.data().price);
-
-                    return 'selected';
-
-                } else if (this.status() == 'selected') {
-
-                    //update the counter
-                    $counter.text(sc.find('selected').length - 1);
-
-                    //and total
-                    $total.text(recalculateTotal(sc) - this.data().price);
-
-                    //remove the item from our cart
-                    $('#cart-item-' + this.settings.id).remove();
-
-                    //seat has been vacated
-                    return 'available';
-
-                } else if (this.status() == 'unavailable') {
-                    //seat has been already booked
-                    return 'unavailable';
-                } else {
-                    return this.style();
-                }
-            }
-        });
-
-    //this will handle "[cancel]" link clicks
-    $('#selected-seats').on('click', '.cancel-cart-item', function() {
-        //let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
-        sc.get($(this).parents('li:first').data('seatId')).click();
-    });
-
-    //let's pretend some seats have already been booked
-    // sc.get(['1_2', '4_1', '7_1', '7_2']).status('unavailable');
-    sc.get(booked).status('unavailable');
-
+  $('#selected-seats').on('click', '.cancel-cart-item', function () {
+      sc.get($(this).parents('li:first').data('seatId')).click();
+  });
 });
 
-function recalculateTotal(sc) {
-    var total = 0;
-
-    //basically find every selected seat and sum its price
-    sc.find('selected').each(function() {
-
-        total += this.data().price;
-
-    });
-
-    return total;
+function recalcularTotal(sc) {
+  var total = 0;
+  sc.find('selected').each(function () {
+      total += this.data().price;
+  });
+  return total;
 }
 
-$(function() {
-    $('#checkout-button').click(function() {
-        var items = $('#selected-seats li')
-        if (items.length <= 0) {
-            alert("Please select atleast 1 seat first.")
-            return false;
-        }
-        var selected = [];
-        items.each(function(e) {
-            var id = $(this).attr('id')
-            id = id.replace("cart-item-", "")
-            selected.push(id)
-        })
-        if (Object.keys(booked).length > 0) {
-            Object.keys(booked).map(k => {
-                selected.push(booked[k])
-            })
-        }
-        localStorage.setItem('booked', JSON.stringify(selected))
-        alert("Seats has been Reserved successfully.")
-        location.reload()
-    })
-    $('#reset-btn').click(function() {
-        if (confirm("are you sure to reset the reservation of the bus?") === true) {
-            localStorage.removeItem('booked')
-            alert("Seats has been Reset successfully.")
-            location.reload()
-        }
-    })
-})
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_PROJECT_ID",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
